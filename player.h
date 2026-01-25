943826a77439a0b0aa5b8bcd611329c13010a657
Tobechukwu Ikenwe othniel <tobechukwu.ikenwe@usm.edu>
2026-01-24 22:46:28 -0600

    Refactor Player struct to class with updated defaults

--- player.h
+++ player.h
@@ -1,18 +1,11 @@
-// Note check map.h for feedback on pragma
-
 #ifndef PLAYER_H
 #define PLAYER_H
 
-
-#pragma once
-#include<cmath>
-
-struct Player{
-    double x = 1.5;
-    double y = 1.5;
+class Player {
+public:
+    double x = 3.5;
+    double y = 3.5;
     double angle = 0.0;
-    static constexpr double FOV = M_PI/3.0;
 };
 
-
-#endif
+#endif // PLAYER_H
