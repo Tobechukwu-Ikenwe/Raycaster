c3811316eb18c3b3c136e421e6c25eb426b53e4e
Tobechukwu Ikenwe othniel <tobechukwu.ikenwe@usm.edu>
2026-01-24 22:47:19 -0600

    Refactor Map class and clean up comments
    Removed redundant comments and updated grid representation.

--- map.h
+++ map.h
@@ -1,48 +1,16 @@
-// NOTE: This is redundant.
-//      You are using a header guard + pragma once 
-//      all you need is pragma once decide which one you prefer to use
 #ifndef MAP_H
 #define MAP_H
 
-#pragma once
-#include<vector>
+#include <array>
 
-// TIP: Keeping everything header and compiling it to one big binary is okay for smaller projects
-// However in bigger projects this will slow down compilation a lot as if you make a minor change the whole binary 
-// will need to be recompiled. A good tip is to start splitting things up into different folders and compile into a binary.
-// Then link it during compilation this way the whole binary does not have to be recompiled if you change something
-// Create a map.h map.c then compile into a map.o then link it with main.o. Look into static linking
+class Map {
+public:
+    static bool isWall(int x, int y);
 
-class Map{
-    static constexpr int width = 16;
+    static constexpr int width  = 16;
     static constexpr int height = 16;
 
-    //1 = wall 0 = empty
-
-    static constexpr int grid[height][width] = {
-        {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
-        {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-        {1,0,1,0,1,1,1,0,1,1,1,0,1,1,0,1},
-        {1,0,1,0,0,0,0,0,0,0,0,0,0,1,0,1},
-        {1,0,1,1,1,1,0,1,1,1,0,1,0,1,0,1},
-        {1,0,0,0,0,0,0,0,0,0,0,1,0,0,0,1},
-        {1,1,1,1,1,1,1,1,1,1,0,1,1,1,0,1},
-        {1,0,0,0,0,0,0,0,0,1,0,0,0,1,0,1},
-        {1,0,1,1,1,1,1,1,0,1,1,1,0,1,0,1},
-        {1,0,0,0,0,0,0,1,0,0,0,0,0,1,0,1},
-        {1,1,1,1,1,1,0,1,1,1,1,1,0,1,0,1},
-        {1,0,0,0,0,0,0,0,0,0,0,1,0,0,0,1},
-        {1,0,1,1,1,1,1,1,1,1,0,1,1,1,0,1},
-        {1,0,0,0,0,0,0,0,0,1,0,0,0,1,0,1},
-        {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-        {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}
-    };
-    public:
-    // Note: this can be rewritten better you can simply just say return grid[y][x] == 1 instead of conditions
-    // Tip: this is a optimization if you know the compiler will never throw add noexcept
-    static bool isWall(int x, int y) noexcept {
-        return grid[y][x] == 1;
-    }
+    static const std::array<std::array<int, width>, height> layout;
 };
 
-#endif
+#endif // MAP_H
